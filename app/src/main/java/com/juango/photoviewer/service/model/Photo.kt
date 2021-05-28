a4960007c3539a0b5377c938a7da092471449925
juango <jgonzalofh2898@gmail.com>
2021-05-28 14:50:04 -0500

    update class to parcealize

--- app/src/main/java/com/juango/photoviewer/service/model/Photo.kt
+++ app/src/main/java/com/juango/photoviewer/service/model/Photo.kt
@@ -1,11 +1,14 @@
 package com.juango.photoviewer.service.model
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.Serializable
 
 @Entity
 @Serializable
+@Parcelize
 data class Photo(
     val albumId: Int,
     @PrimaryKey
@@ -13,4 +16,4 @@ data class Photo(
     val title: String,
     val url: String,
     val thumbnailUrl: String
-)
+) : Parcelable
